63869b0810fa641a38ddb7c6eb70e78f8500cea1
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-03-30 12:06:06 +0200

    set correct path

--- backend/src/main/resources/sql/seedDatabase/seed.sh
+++ backend/src/main/resources/sql/seedDatabase/seed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-psql "postgresql://tesco_db_user:1Se90zNOSMBwbun01OSdh1aPapS73vZc@dpg-d6oucd75r7bs73f648d0-a.frankfurt-postgres.render.com:5432/tesco_db?sslmode=require" \
+psql "env" \
      -f seed_database.sql
